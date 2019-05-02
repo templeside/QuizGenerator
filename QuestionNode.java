a68f9e06b13ad67c1627d44164fb388f7f711fc0
schaines <47762076+schaines@users.noreply.github.com>
2019-05-01 19:31:03 -0500

    Update QuestionNode.java

--- QuestionNode.java
+++ QuestionNode.java
@@ -6,24 +6,37 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
+/**
+  *This is the QuestionNode class.
+  */
 public class QuestionNode {
     
     private VBox node;
     private ToggleGroup choices;
     
+    /**
+	 *This is the constructor for the class.
+     *@param q is the Question used to make the QuestionNode
+	 */
     public QuestionNode(Question q) {
         Image image = new Image(q.getImage());
         ImageView imageView = new ImageView(image);
         Label questionLb = new Label(q.getQuestion());
         choices = (ToggleGroup) q.getChoices();
         
-        node.getChildren().addAll(imageView, questionLb);
+        node.getChildren().addAll(imageView, questionLb);//adds image and question to node
     }
     
+    /**
+	 *@return node
+	 */
     public VBox getNode() {
         return this.node;
     }
     
+    /**
+	 *@return choices
+	 */
     public ToggleGroup getChoices() {
         return this.choices;
     }

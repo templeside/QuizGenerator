80cad8ab146215787cb664907b2f0f7793be0ce1
schaines <47762076+schaines@users.noreply.github.com>
2019-05-01 19:15:35 -0500

    Update Choice.java

--- Choice.java
+++ Choice.java
@@ -1,19 +1,30 @@
 package application;
-
+/**
+  *This is the Choice class.
+  */
 public class Choice {
 
     public boolean isCorrect;
     public String choice;
     
+    /**
+	 *This is the constructor for the class.
+	 */
     public Choice(boolean correctAnswer, String newChoice) {
         this.isCorrect = correctAnswer;
         this.choice = newChoice;
     }
     
+    /**
+	 *@return the choice.
+	 */
     public String getChoice() {
         return this.choice;
     }
     
+    /**
+	 *@return true if the choice is correct, false if the choice is incorrect
+	 */
     public boolean getIsCorrect() {
         return this.isCorrect;
     }

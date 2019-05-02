d4ac7dc2ed689136292a19880cbcc06c7fce6cbe
schaines <47762076+schaines@users.noreply.github.com>
2019-05-01 19:07:58 -0500

    Update Question.java

--- Question.java
+++ Question.java
@@ -2,6 +2,9 @@ package application;
 
 import java.util.List;
 
+/**
+  *This is the Question class.
+  */
 public class Question {
     private String metadata;
     private String question;
@@ -10,6 +13,9 @@ public class Question {
     private List<Choice> choices;
     private String answer;
 
+    /**
+	 *This is the constructor for the class.
+	 */
     public Question(String newMetadata, String newQuestion, String newTopic, String newImage,
         List<Choice> newChoices, String newAnswer) {
         this.metadata = newMetadata;
@@ -20,26 +26,44 @@ public class Question {
         this.answer = newAnswer;
     }
 
+    /**
+	 *@return the question
+	 */
     public String getQuestion() {
         return this.question;
     }
 
+    /**
+	 *@return the choices for the Question
+	 */
     public List<Choice> getChoices() {
         return this.choices;
     }
 
+    /**
+	 *@return the answer for the Question
+	 */
     public String getAnswer() {
         return this.answer;
     }
 
+    /**
+	 *@return the metadata for the Question
+	 */
     public String getMetadata() {
         return this.metadata;
     }
 
+    /**
+	 *@return the topic for the Question
+	 */
     public String getTopic() {
         return this.topic;
     }
 
+    /**
+	 *@return the image for the Question
+	 */
     public String getImage() {
         return this.image;
     }

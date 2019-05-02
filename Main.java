33da60c92c5c5a54e8064b9aa366f4a8981f00bd
jabbarifard <47994915+jabbarifard@users.noreply.github.com>
2019-05-01 21:14:40 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,18 +1,33 @@
 package application;
 
+import java.awt.Desktop;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.List;
+
+import javax.swing.event.ChangeListener;
+
 import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -31,6 +46,8 @@ public class Main extends Application {
         try {
             primaryStage.setFullScreen(true);
             // top
+            FileChooser fileChooser = new FileChooser(); 
+            Desktop desktop = Desktop.getDesktop();
             Label topLabel = new Label("Welcome to Quiz Generator");
             topLabel.setFont(Font.font("Amble CN", FontWeight.BOLD, 20));
             BorderPane root = new BorderPane();
@@ -54,14 +71,190 @@ public class Main extends Application {
             Button btn1 = new Button();
             btn1.setText("Add a question");
             leftVB.getChildren().add(btn1);
+            EventHandler<ActionEvent> event1 = new EventHandler<ActionEvent>() { 
+                public void handle(ActionEvent e) 
+                {
+                	VBox questionBox = new VBox();
+                	Label numQLabel = new Label("Enter your question: ");
+                    TextField textField = new TextField();
+                    HBox hb = new HBox();
+                    hb.getChildren().addAll(numQLabel, textField);
+                    hb.setSpacing(10);
+                    questionBox.getChildren().addAll(hb);
+                    //metadata
+                    Label metaLabel = new Label("Enter your Metadata:");
+                    TextField textField2 = new TextField();
+                    HBox hb2 = new HBox();
+                    hb2.getChildren().addAll(metaLabel, textField2);
+                    hb2.setSpacing(10);
+                    questionBox.getChildren().addAll(hb2);
+                    //topic
+                    Label topicLabel = new Label("Enter your topic:");
+                    TextField textField3 = new TextField();
+                    HBox hb3 = new HBox();
+                    hb3.getChildren().addAll(topicLabel, textField3);
+                    hb3.setSpacing(10);
+                    questionBox.getChildren().addAll(hb3);
+                    //image
+                    Label imageLabel = new Label("Enter Image File name:");
+                    TextField textField4 = new TextField();
+                    HBox hb4 = new HBox();
+                    hb4.getChildren().addAll(imageLabel, textField4);
+                    hb4.setSpacing(10);
+                    questionBox.getChildren().addAll(hb4);
+                    //adding choice1
+                    
+                    Label choice1Label = new Label("Choice1:");
+                    TextField textField5 = new TextField();
+                    HBox hb5 = new HBox();
+                    hb5.getChildren().addAll(choice1Label, textField5);
+                    hb5.setSpacing(10);
+                    questionBox.getChildren().addAll(hb5);
+                    
+                    // create radiobuttons
+                    VBox r = new VBox(); 
+                    RadioButton r1 = new RadioButton("True"); 
+                    RadioButton r2 = new RadioButton("False"); 
+                    ToggleGroup tg = new ToggleGroup(); 
+                    
+                    r1.setToggleGroup(tg); 
+                    r2.setToggleGroup(tg);
+                    r.getChildren().addAll(r1, r2);
+                    
+                    questionBox.getChildren().addAll(r);
+          
+
+                    
+                    //add choice2
+                    Label choice2Label = new Label("Choice2:");
+                    TextField textField6 = new TextField();
+                    HBox hb6 = new HBox();
+                    hb6.getChildren().addAll(choice2Label, textField6);
+                    hb6.setSpacing(10);
+                    questionBox.getChildren().addAll(hb6);
+                    
+                    // create radiobuttons
+                    VBox rr = new VBox();   
+                    RadioButton rr1 = new RadioButton("True"); 
+                    RadioButton rr2 = new RadioButton("False"); 
+                    ToggleGroup tg2 = new ToggleGroup(); 
+                    
+                    rr1.setToggleGroup(tg2); 
+                    rr2.setToggleGroup(tg2);
+                    rr.getChildren().addAll(rr1, rr2); 
+
+                    questionBox.getChildren().addAll(rr);
+                    
+                  //add choice3
+                              
+                    Label choice3Label = new Label("Choice3:");
+                    TextField textField7 = new TextField();
+                    HBox hb7 = new HBox();
+                    hb7.getChildren().addAll(choice3Label, textField7);
+                    hb7.setSpacing(10);
+                    questionBox.getChildren().addAll(hb7);
+                    
+                    // create radiobuttons
+                    VBox rrr = new VBox();   
+                    RadioButton rrr1 = new RadioButton("True"); 
+                    RadioButton rrr2 = new RadioButton("False"); 
+                    ToggleGroup tg3 = new ToggleGroup(); 
+                    
+                    rrr1.setToggleGroup(tg3); 
+                    rrr2.setToggleGroup(tg3);
+                    rrr.getChildren().addAll(rrr1,rrr2); 
+
+                    questionBox.getChildren().addAll(rrr);
+                    
+                    Button addQuestionBtn = new Button("Add Question");
+                    addQuestionBtn.setOnAction(new EventHandler<ActionEvent>() {
+                    	
+                    	@Override
+                    	public void handle(ActionEvent event) {
+                    		
+//                    		1. Here, get all the text from the respective textfields like question test, choices
+//                    		and everything else.
+//                    		2. Create a new Question object
+//                    		3. add the question object to the QuestionDatabase
+                    	}
+                    });
+                    
+                    questionBox.getChildren().addAll(addQuestionBtn);
+                    
+                    
+//                    1. create a new button for saving the question
+//                    2. do the following steps in the button onclick event handler
+//                    3. get all the question details from the question box fields - TextField
+//                    4. Create a new Question object
+//                    5. Add Question object to QuestionDatabase
+//                    6. At the end we need a button to go back to the main page
+                    
+                    Scene questionScene = new Scene(questionBox, 400, 400);                	
+                	primaryStage.setScene(questionScene);
+                    primaryStage.show();
+               }
+            };
+         // when button is pressed 
+            btn1.setOnAction(event1); 
 
             Button btn2 = new Button();
             btn2.setText("Load a json file");
             leftVB.getChildren().add(btn2);
+            EventHandler<ActionEvent> event2 = new EventHandler<ActionEvent>() { 
+                public void handle(ActionEvent e) 
+                {
+                	
+                	File file = fileChooser.showOpenDialog(primaryStage);
+                	String output = "file path";
+                	StringBuffer stringBuffer = new StringBuffer();
+                    if (file != null) {
+                    	try {
+                    		FileReader fileReader = new FileReader(file);
+                			BufferedReader bufferedReader = new BufferedReader(fileReader);
+                			
+                			String line;
+                			while ((line = bufferedReader.readLine()) != null) {
+                				stringBuffer.append(line);
+                				stringBuffer.append("\n");
+                				System.out.println(line);
+                			}
+                			fileReader.close();
+                    	}
+                    	catch(Exception ex)
+                    	{
+                    		ex.printStackTrace();
+                    	}
+                    }
+                	VBox jsonBox = new VBox();
+                	Label newJson = new Label();
+                	newJson.setText(stringBuffer.toString());
+
+                	jsonBox.getChildren().add(newJson);
+                	Scene jsonScene = new Scene(jsonBox, 400, 400);                	
+                	primaryStage.setScene(jsonScene);
+                    primaryStage.show();
+               }
+            };
+         // when button is pressed 
+            btn2.setOnAction(event2); 
 
             Button btn3 = new Button();
             btn3.setText("Save the current questions to a json file");
             leftVB.getChildren().add(btn3);
+            EventHandler<ActionEvent> event3 = new EventHandler<ActionEvent>() { 
+                public void handle(ActionEvent e) 
+                {
+                	VBox questionBox = new VBox();
+                	Label newQuestion = new Label();
+                	newQuestion.setText("Enter the question");
+                	questionBox.getChildren().add(newQuestion);
+                	Scene questionScene = new Scene(questionBox, 400, 400);                	
+                	primaryStage.setScene(questionScene);
+                    primaryStage.show();
+               }
+            };
+         // when button is pressed 
+            btn3.setOnAction(event3); 
 
             root.setLeft(leftVB);
 

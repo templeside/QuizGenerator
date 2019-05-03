ca63e4a33706a763888c4df4f94b14127b25a247
schaines <47762076+schaines@users.noreply.github.com>
2019-05-03 15:21:17 -0500

    Update AddQuestionFormNode.java

--- AddQuestionFormNode.java
+++ AddQuestionFormNode.java
@@ -1,31 +1,32 @@
-//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION ////////////////////////
-// Title:           ATEAM PROJECT                    							//
-// Files:           AddQuestionFormNode, Choice, Main, QuestionNode             //
-//                  Question, QuestionDatabase, QuestionDatabaseADT	        	//	
-// Course:          CS 400, Spring 2018						     				//
-// Author:          ATEAM106 (Andrew Lutkus, Chanwoong Jhon,                    //
-//                            Elaheh Jabbarifard, Chaiyeen Oh, Sara Haines)     //
-// Email:           alutkus@wisc.edu, cjhon@wisc.edu,                           //
-//                  jabbarifard@wisc.edu, coh26@wisc.edu, schaines@wisc.edu     //
-// Lecturer's Name: Andrew Kuemmel, Deb Deppeler								//
-// Due Date : 5/2/2019 10pm														//
-// 																				//
-// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:			//
-//   _X_ Write-up states that pair programming is allowed for this assignment.	//
-//   _X_ We have both read and understand the course Pair Programming Policy.	//
-//   _X_ We have registered our team prior to the team registration deadline.	//
-//																				//
+
+////////////////////ALL ASSIGNMENTS INCLUDE THIS SECTION ////////////////////////
+//Title:           ATEAM PROJECT                    							//
+//Files:           AddQuestionFormNode, Choice, Main, QuestionNode             //
+//Question, QuestionDatabase, QuestionDatabaseADT	        	//	
+//Course:          CS 400, Spring 2018						     				//
+//Author:          ATEAM106 (Andrew Lutkus, Chanwoong Jhon,                    //
+//       Elaheh Jabbarifard, Chaiyeen Oh, Sara Haines)     //
+//Email:           alutkus@wisc.edu, cjhon@wisc.edu,                           //
+//jabbarifard@wisc.edu, coh26@wisc.edu, schaines@wisc.edu     //
+//Lecturer's Name: Andrew Kuemmel, Deb Deppeler								//
+//Due Date : 5/2/2019 10pm														//
+//															//
+//VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:			//
+//_X_ Write-up states that pair programming is allowed for this assignment.	//
+//_X_ We have both read and understand the course Pair Programming Policy.	//
+//_X_ We have registered our team prior to the team registration deadline.	//
+//															//
 ///////////////////////////// CREDIT OUTSIDE HELP ////////////////////////////////
-//																				//
-// Students who get help from sources other than their partner must fully 		//
-// acknowledge and credit those sources of help here.  Instructors and TAs do 	//
-// not need to be credited here, but tutors, friends, relatives, room mates, 	//
-// strangers, and others do.  If you received no outside help from either type	//
-//  of source, then please explicitly indicate NONE.							//
-//																				//
-// Persons:         X															//
-// Online Sources:  X															//
-//																				//
+//															//
+//Students who get help from sources other than their partner must fully 		//
+//acknowledge and credit those sources of help here.  Instructors and TAs do 	//
+//not need to be credited here, but tutors, friends, relatives, room mates, 	//
+//strangers, and others do.  If you received no outside help from either type	//
+//of source, then please explicitly indicate NONE.							//
+//															//
+//Persons:         X															//
+//Online Sources:  X															//
+//															//
 /////////////////////////////// 80 COLUMNS WIDE //////////////////////////////////
 package application;
 
@@ -37,7 +38,6 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import javafx.scene.Node;
-import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
@@ -45,80 +45,59 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class AddQuestionFormNode {
-    
-    private List<Choice> choiceTexts;
-    private List<Boolean> choiceGroups;
-    private VBox form;
-    
-    public AddQuestionFormNode() {       
-        choiceTexts = new ArrayList<Choice>();
-        choiceGroups = new ArrayList<Boolean>();
-        form = new VBox();
-    }
-    
-    public String getQuestion() {
-    	return ((HBox)form.getChildren().get(0)).getChildren().get(1).toString();               
-    }
-    
-    public String getMetadata() {
-    	
-        return ((HBox)form.getChildren().get(1)).getChildren().get(1).toString();
-    }    
 
-    
-    public String getTopic() {
-    	return ((HBox)form.getChildren().get(2)).getChildren().get(1).toString();        
-    }
+	private List<String> choiceTexts;
+	private List<Boolean> choiceGroups;
+	private VBox form;
 
-    public String getImage() {
-    	return form.getChildren().get(3).toString();        
-    }
-    
-    public List<Choice> getChoiceTexts() {    	
-    	
-        //textfield5가 들어있는곳.
-        //hb5는 hbox이다
-//        hb5.getChildren().addAll(choice1Label, textField5);
-//        questionBox.getChildren().addAll(hb5);
-        
-        // create radiobuttons
-//        VBox r = new VBox(); 
-//        RadioButton r1 = new RadioButton("True"); 
-//        RadioButton r2 = new RadioButton("False"); 
-//        ToggleGroup tg = new ToggleGroup(); 
-//        
-//        r1.setToggleGroup(tg); 
-//        r2.setToggleGroup(tg);
-//        r.getChildren().addAll(r1, r2);
-//        
-//        questionBox.getChildren().addAll(r);
-        
-    	int i;
-    	for(i = 0;i<5;i++) {
-    	HBox hb = (HBox) form.getChildren().get((i*2)+4);
-    	
-    	
-    	
-    	Choice choice = new Choice(this.getChoiceGroups().get(i),hb.getChildren().get(1).toString());
-    	choiceTexts.add(choice);
-    	}
-        return choiceTexts;
-        
-        
-    }
-    
-    public VBox getNode() {
-    	return form;
-        
-    }
-    
-    public List<Boolean> getChoiceGroups() {
-    	for(int i = 0;i<5;i++) {
-    		VBox newbox = ((VBox)form.getChildren().get((i*2)+5));
-    		
-        	RadioButton radioTrue =  (RadioButton)newbox.getChildren().get(0);
-        	choiceGroups.add(radioTrue.isSelected());     
-    	}
-    	return choiceGroups;
-    }
+	public AddQuestionFormNode() {
+		choiceTexts = new ArrayList<String>();
+		choiceGroups = new ArrayList<Boolean>();
+		form = new VBox();
+	}
+
+	public TextField getQuestion() {
+		return (TextField) form.getChildren().get(0);
+
+	}
+
+	public TextField getMetadata() {
+
+		return (TextField) form.getChildren().get(1);
+
+	}
+
+	public TextField getTopic() {
+		return (TextField) form.getChildren().get(2);
+
+	}
+
+	public TextField getImage() {
+		return (TextField) form.getChildren().get(3);
+
+	}
+
+	public List<String> getChoiceTexts() {
+		int i;
+		for (i = 4; i <= 19; i = i + 3) {
+			HBox hb = (HBox) form.getChildren().get(i);
+			choiceTexts.add((String) (hb.getChildren().get(1)).toString());
+		}
+		return choiceTexts;
+
+	}
+
+	public VBox getNode() {
+		return form;
+
+	}
+
+	public List<Boolean> getChoiceGroups() {
+		for (int i = 5; i <= 19; i = i + 3) {
+			RadioButton radioTrue = (RadioButton) form.getChildren().get(i);
+			choiceGroups.add(radioTrue.isSelected());
+		}
+		return choiceGroups;
+	}
+
 }

5dc730139a9aa808dcf1b454c67fefc4eb70b9a8
schaines <47762076+schaines@users.noreply.github.com>
2019-05-03 16:50:21 -0500

    Update Choice.java

--- Choice.java
+++ Choice.java
@@ -34,8 +34,8 @@ package application;
  */
 public class Choice {
 	//fields
-	public boolean isCorrect;
-	public String choice;
+	public boolean isCorrect;//true if choice is correct, false if incorrect
+	public String choice;//the typed out choice
 
 	/**
 	 *This is the constructor for the class.

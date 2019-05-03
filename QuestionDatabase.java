9c165e6ed87b9ca931c0f6f284e92712f5f5b515
templeside <47994862+templeside@users.noreply.github.com>
2019-05-03 00:45:12 -0500

    Add files via upload

--- QuestionDatabase.java
+++ QuestionDatabase.java
@@ -1,3 +1,32 @@
+//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION ////////////////////////
+// Title:           ATEAM PROJECT                    							//
+// Files:           AddQuestionFormNode, Choice, Main, QuestionNode             //
+//                  Question, QuestionDatabase, QuestionDatabaseADT	        	//	
+// Course:          CS 400, Spring 2018						     				//
+// Author:          ATEAM106 (Andrew Lutkus, Chanwoong Jhon,                    //
+//                            Elaheh Jabbarifard, Chaiyeen Oh, Sara Haines)     //
+// Email:           alutkus@wisc.edu, cjhon@wisc.edu,                           //
+//                  jabbarifard@wisc.edu, coh26@wisc.edu, schaines@wisc.edu     //
+// Lecturer's Name: Andrew Kuemmel, Deb Deppeler								//
+// Due Date : 5/2/2019 10pm														//
+// 																				//
+// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:			//
+//   _X_ Write-up states that pair programming is allowed for this assignment.	//
+//   _X_ We have both read and understand the course Pair Programming Policy.	//
+//   _X_ We have registered our team prior to the team registration deadline.	//
+//																				//
+///////////////////////////// CREDIT OUTSIDE HELP ////////////////////////////////
+//																				//
+// Students who get help from sources other than their partner must fully 		//
+// acknowledge and credit those sources of help here.  Instructors and TAs do 	//
+// not need to be credited here, but tutors, friends, relatives, room mates, 	//
+// strangers, and others do.  If you received no outside help from either type	//
+//  of source, then please explicitly indicate NONE.							//
+//																				//
+// Persons:         X															//
+// Online Sources:  X															//
+//																				//
+/////////////////////////////// 80 COLUMNS WIDE //////////////////////////////////
 package application;
 
 import java.io.File;
@@ -7,49 +36,52 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+/**
+ * Contains all questions from .json files and added manually by user
+ * @author ateam 106
+ *
+ */
 public class QuestionDatabase implements QuestionDatabaseADT {
 
-    private Map<String, List<Question>> topics;
+	//fields
+	private Map<String, List<Question>> topics;
 
-    public QuestionDatabase() {
-        topics = new HashMap<String, List<Question>>();
-    }
+	//no arg constructor 
+	public QuestionDatabase() {
+		topics = null;
+	}
 
-    public void addQuestion(Question q) {
-        List<Question> addQuestionList = new ArrayList<Question>();
-        addQuestionList.add(q);
-        if (topics.containsKey(q.getTopic())) {
-            topics.get(q.getTopic()).addAll(addQuestionList);
-        } else {
-            topics.put(q.getTopic(), addQuestionList);
-        }
+	/**
+	 * Adds the questions given by the user with topic and question
+	 * @param topic
+	 * @param q
+	 */
+	public void addQuestion(String topic, Question q) {
+		topics.get(topic).add(q); // new question added into list of questions
+	}
 
-    }
+	/**
+	 * Returns the number of questions that are already in List of questions
+	 * @return
+	 */
+	public int getNumQuestions() {
+		int num = 0;
+		for(int i = 0; i < topics.size(); i++) {
+			num+= topics.get(i).size();
+		}
+		return num; // number of questions in "topics"
+	}
 
-    public int getNumQuestions() {
-        int count = 0;
-        List keys = new ArrayList(topics.keySet());
-        for (int i = 0; i < keys.size(); i++) {
-            for (int j = 0; j < topics.get(keys.get(i)).size(); j++) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    /**
+	/**
 	 * This method saves all the questions from the "topics" (List of questions) into the json file 
 	 *@param file is the file that the questions are saved to.
 	 */
@@ -80,76 +112,60 @@ public class QuestionDatabase implements QuestionDatabaseADT {
 		}
 	}
 
-/**
+	/**
 	 *This method returns a list of Questions given their topic.
 	 *@param topic is the topic of the Questions
 	 */
-    public List<Question> getQuestions(String topic) {
-        List<Question> questionList = new ArrayList<Question>();
-        questionList = topics.get(topic);
-        return questionList;
+	public List<Question> getQuestions(String topic) {
+		return topics.get(topic);
+	}
 
-    }
-    
-/**
+	/**
 	 *This method loads Questions from a JSON file.
 	 *@param file is the file that Questions are being loaded from
 	 */
-    public void loadQuestionsFromJSON(File file) {
-        Object obj;
+	public void loadQuestionsFromJSON(File file) {
+		Object obj;
 
-        try {
-            obj = new JSONParser().parse(new FileReader(file));
-            JSONObject jo = (JSONObject) obj;
-            JSONArray questionArr = (JSONArray) jo.get("questionArray");
-            List<Choice> choiceList = new ArrayList<Choice>();
-            List<Question> questionList = new ArrayList<Question>();
-            Choice corrChoice = null;
+		try {
+			obj = new JSONParser().parse(new FileReader(file));
+			JSONObject jo = (JSONObject) obj;
+			JSONArray questionArr = (JSONArray) jo.get("questionArray");
+			List<Choice> choiceList = new ArrayList<Choice>();
+			Choice corrChoice = null;
 
-            for (int i = 0; i < questionArr.size(); i++) {
-                JSONObject qo = (JSONObject) questionArr.get(i);
-                String metadata = (String) qo.get("meta-data");
-                String question = (String) qo.get("questionText");
-                String topic = (String) qo.get("topic");
-                String image = (String) qo.get("image");
-                JSONArray choiceArr = (JSONArray) qo.get("choiceArray");
-                for (int j = 0; j < choiceArr.size(); j++) {
-                    JSONObject co = (JSONObject) choiceArr.get(j);
-                    String isCorr = (String) co.get("isCorrect");
-                    String choiceText = (String) co.get("choice");
-                    if (isCorr.equals("T")) {
-                        Choice newChoice = new Choice(true, choiceText);
-                        corrChoice = newChoice;
-                        choiceList.add(newChoice);                  
-                    } else {
-                        Choice newChoice = new Choice(false, choiceText);
-                        choiceList.add(newChoice);
-                    }
-                }
-                Question newQuestion =
-                    new Question(metadata, question, topic, image, choiceList, corrChoice.choice);
-                questionList.add(newQuestion);
-                if (topics.containsKey(newQuestion.getTopic())) {
-                    topics.get(newQuestion.getTopic()).addAll(questionList);
-                } else {
-                    topics.put(newQuestion.getTopic(), questionList);
-                }
-                choiceList.clear();
-            }
+			for (int i = 0; i < questionArr.size(); i++) {
+				JSONObject qo = (JSONObject) questionArr.get(i);
+				String metadata = (String) qo.get("meta-data");
+				String question = (String) qo.get("questionText");
+				String topic = (String) qo.get("topic");
+				String image = (String) qo.get("image");
+				JSONArray choiceArr = (JSONArray) qo.get("choiceArray");
+				for (int j = 0; j < choiceArr.size(); j++) {
+					JSONObject co = (JSONObject) choiceArr.get(i);
+					boolean isCorr = (boolean) co.get("isCorrect");
+					String choiceText = (String) co.get("choice");
+					Choice newChoice = new Choice(isCorr, choiceText);
+					if (isCorr == true) {
+						corrChoice = newChoice;
+					}
+					choiceList.add(newChoice);
+				}
 
-        } catch (IOException | ParseException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-    
-/**
+				Question newQuestion =
+						new Question(metadata, question, topic, image, choiceList, corrChoice.choice);
+				choiceList.clear();
+			}
+
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
 	 *@return an ObservableList of the topics
 	 */
-    public ObservableList<String> getTopics() {
-        ObservableList<String> list = FXCollections.observableArrayList();
-        list.addAll(topics.keySet());
-        java.util.Collections.sort(list);
-        return list;
-    }
+	public ObservableList<String> getTopics() {
+		return null;
+	}
 }
